3b9f1c02b67fe7ab0c364b89a582a066ea206ec3
rafirahman123 <76124938+rafirahman123@users.noreply.github.com>
2021-10-31 04:02:58 +0600

    some style change in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import useAuth from '../../hook/useAuth';
 import logo from '../../image/react-logo-svgrepo-com.svg';
+import './Header.css';
 
 const Header = () => {
     const { user, logOut } = useAuth();
@@ -34,8 +35,25 @@ const Header = () => {
                         </Nav>
                         {user?.email ?
 
+
                             <nav>
-                                <NavLink className="mx-1" to="/manageAllOrder">
+                                <NavDropdown title="Others Menu" id="collasible-nav-dropdown" className="mx-1 btn btn-outline-secondary text-white">
+                                    <NavDropdown.Item href="#action/3.1">
+                                        <NavLink className="mx-1" to="/manageAllOrder">
+                                            <Button variant="outline-secondary">Manage All Order</Button>
+                                        </NavLink></NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2"><NavLink className="mx-1" to="/myOrders">
+                                        <Button variant="outline-secondary">Myorders</Button>
+                                    </NavLink></NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3"><NavLink className="mx-1" to="/addPackage">
+                                        <Button variant="outline-secondary">Addpackage</Button>
+                                    </NavLink></NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4"><button onClick={logOut} type="button" className="btn btn-outline-secondary mx-1 justify-content-end ">Logout</button></NavDropdown.Item>
+                                </NavDropdown>
+
+
+                                {/* <NavLink className="mx-1" to="/manageAllOrder">
                                     <Button variant="outline-secondary">Manage All Order</Button>
                                 </NavLink>
                                 <NavLink className="mx-1" to="/myOrders">
@@ -44,17 +62,18 @@ const Header = () => {
                                 <NavLink className="mx-1" to="/addPackage">
                                     <Button variant="outline-secondary">Addpackage</Button>
                                 </NavLink>
-                                <button onClick={logOut} type="button" className="btn btn-outline-secondary mx-1 justify-content-end ">Logout</button>
+                                <button onClick={logOut} type="button" className="btn btn-outline-secondary mx-1 justify-content-end ">Logout</button> */}
                             </nav>
                             :
 
                             <NavLink to="/login">
-                                <button type="button" className="btn btn-outline-secondary ">Login</button>
+                                <button type="button" className="btn btn-outline-secondary ">Sign In</button>
                             </NavLink>
                         }
-                        <Navbar.Text>
-                            <span className="text-secondary">Signed in as:</span> <a href="#login">{user?.displayName}</a>
-                        </Navbar.Text>
+                        {user?.email ?
+                            <Navbar.Text>
+                                <span className="text-secondary"></span> <img className='profile-image' src={user?.photoURL} alt='' /> <a href="#login">{user?.displayName}</a>
+                            </Navbar.Text> : ''}
                     </Navbar.Collapse>
 
                 </Container>
